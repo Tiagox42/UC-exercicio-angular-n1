7b53235d0bb04778def44515d3b99731a876d478
Tiago Dantas <tiagodantas42@gmail.com>
2021-04-17 18:12:13 -0300

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { Cliente } from './cliente/cliente.model';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'UC-exercicio-angular-n1';
+  title = 'form-tutorial';
+  cliente: Cliente = new Cliente();
+  constructor(private http: HttpClient) {}
+  VerificaCEP() {
+    this.http
+      .get(`https://viacep.com.br/ws/${this.cliente.CEP}/json/`)
+      .toPromise()
+      .then((data) => {
+        const dado: any = data;
+        this.cliente.Endereco = dado.logradouro;
+      });
+  }
+  salvar() {
+    console.log(this.cliente);
+  }
 }
